1e4bc141106df47e741ee9897a9d812f0f9fedc6
nofre <nodiek@gmail.com>
2017-01-22 17:42:25 +0100

    UI improvements

--- ServerAndWebapp/public/scripts/dashboardCtrl.js
+++ ServerAndWebapp/public/scripts/dashboardCtrl.js
@@ -29,154 +29,163 @@ function refreshData(data_in) {
 		data["t2"][i] = [i, data_in[i]["t2"]];
 		data["light"][i] = [i, data_in[i]["light"]];
 
-		data["timestamp"][i] = i;
+		data["timestamp"][i] =  [i, data_in[i]["date"]];
 	}
 
-	$.plot($("#ext-humidity-chart1"),
-		[{
-			data : data["exth1"],
-			label : "Exterior humidity 1",
-			color: "#0a3683"
-		}],
-		{
-			series: {
-				lines : {
-					show : true,
-					fill : true,
-					lineWidth : 2,
-					fillColor : {
-						colors : [{
-							opacity : 0.05
-						}, {
-							opacity : 0.05
-						}]
+	if ($("#checkexth1").prop("checked")) {
+		$("#ext-humidity-chart1-layout").show();
+		$.plot($("#ext-humidity-chart1"),
+			[{
+				data : data["exth1"],
+				label : "Exterior humidity 1",
+				color: "#0a3683"
+			}],
+			{
+				series: {
+					lines : {
+						show : true,
+						fill : true,
+						lineWidth : 2,
+						fillColor : {
+							colors : [{
+								opacity : 0.05
+							}, {
+								opacity : 0.05
+							}]
+						}
+					},
+					points: {
+						show: false
+					},
+					shadowSize : 0
+				},
+				grid : {
+					hoverable : true,
+					clickable : true,
+					borderColor : "#f9f9f9",
+					tickColor : "#AAAAAA",
+					borderWidth : 1,
+					labelMargin : 10,
+					backgroundColor : "#fff"
+				},
+				legend : {
+					position : "ne",
+					margin : [0, -24],
+					noColumns : 0,
+					labelBoxBorderColor : null,
+					labelFormatter : function(label, series) {
+						// just add some space to labes
+						return '' + label + '&nbsp;&nbsp;';
+					},
+					width : 30,
+					height : 2
+				},
+				yaxis : {
+					tickColor : '#f5f5f5',
+					tickDecimals: 0,
+					font : {
+						color : '#bdbdbd'
 					}
 				},
-				points: {
-					show: false
-				},
-				shadowSize : 0
-			},
-			grid : {
-				hoverable : true,
-				clickable : true,
-				borderColor : "#f9f9f9",
-				tickColor : "#AAAAAA",
-				borderWidth : 1,
-				labelMargin : 10,
-				backgroundColor : "#fff"
-			},
-			legend : {
-				position : "ne",
-				margin : [0, -24],
-				noColumns : 0,
-				labelBoxBorderColor : null,
-				labelFormatter : function(label, series) {
-					// just add some space to labes
-					return '' + label + '&nbsp;&nbsp;';
-				},
-				width : 30,
-				height : 2
-			},
-			yaxis : {
-				tickColor : '#f5f5f5',
-				tickDecimals: 0,
-				font : {
-					color : '#bdbdbd'
-				}
-			},
-			xaxis : {
-				tickColor : '#f5f5f5',
-				ticks: data["timestamp"],
-				rotateTicks: 45,
-				font : {
-					color : '#bdbdbd'
-				}
-			},
-			tooltip : true,
-			tooltipOpts : {
-				content : '%s: %y',
-				shifts : {
-					x : -60,
-					y : 25
-				},
-				defaultTheme : false
-			}
-		}
-	);
-
-	$.plot($("#ext-humidity-chart2"),
-		[{
-			data : data["exth2"],
-			label : "Exterior humidity 2",
-			color: "#0a3683"
-		}],
-		{
-			series: {
-				lines : {
-					show : true,
-					fill : true,
-					lineWidth : 2,
-					fillColor : {
-						colors : [{
-							opacity : 0.05
-						}, {
-							opacity : 0.05
-						}]
+				xaxis : {
+					tickColor : '#f5f5f5',
+					ticks: data["timestamp"],
+					rotateTicks: 45,
+					font : {
+						color : '#bdbdbd'
 					}
 				},
-				points: {
-					show: false
-				},
-				shadowSize : 0
-			},
-			grid : {
-				hoverable : true,
-				clickable : true,
-				borderColor : "#f9f9f9",
-				tickColor : "#AAAAAA",
-				borderWidth : 1,
-				labelMargin : 10,
-				backgroundColor : "#fff"
-			},
-			legend : {
-				position : "ne",
-				margin : [0, -24],
-				noColumns : 0,
-				labelBoxBorderColor : null,
-				labelFormatter : function(label, series) {
-					// just add some space to labes
-					return '' + label + '&nbsp;&nbsp;';
-				},
-				width : 30,
-				height : 2
-			},
-			yaxis : {
-				tickColor : '#f5f5f5',
-				tickDecimals: 0,
-				font : {
-					color : '#bdbdbd'
+				tooltip : true,
+				tooltipOpts : {
+					content : '%s: %y',
+					shifts : {
+						x : -60,
+						y : 25
+					},
+					defaultTheme : false
 				}
-			},
-			xaxis : {
-				tickColor : '#f5f5f5',
-				ticks: data["timestamp"],
-				rotateTicks: 45,
-				font : {
-					color : '#bdbdbd'
-				}
-			},
-			tooltip : true,
-			tooltipOpts : {
-				content : '%s: %y',
-				shifts : {
-					x : -60,
-					y : 25
-				},
-				defaultTheme : false
 			}
-		});
+		);
+	} else $("#ext-humidity-chart1-layout").hide();
 
+	if ($("#checkexth2").prop("checked")) {
+		$("#ext-humidity-chart2-layout").show();
+		$.plot($("#ext-humidity-chart2"),
+			[{
+				data : data["exth2"],
+				label : "Exterior humidity 2",
+				color: "#0a3683"
+			}],
+			{
+				series: {
+					lines : {
+						show : true,
+						fill : true,
+						lineWidth : 2,
+						fillColor : {
+							colors : [{
+								opacity : 0.05
+							}, {
+								opacity : 0.05
+							}]
+						}
+					},
+					points: {
+						show: false
+					},
+					shadowSize : 0
+				},
+				grid : {
+					hoverable : true,
+					clickable : true,
+					borderColor : "#f9f9f9",
+					tickColor : "#AAAAAA",
+					borderWidth : 1,
+					labelMargin : 10,
+					backgroundColor : "#fff"
+				},
+				legend : {
+					position : "ne",
+					margin : [0, -24],
+					noColumns : 0,
+					labelBoxBorderColor : null,
+					labelFormatter : function(label, series) {
+						// just add some space to labes
+						return '' + label + '&nbsp;&nbsp;';
+					},
+					width : 30,
+					height : 2
+				},
+				yaxis : {
+					tickColor : '#f5f5f5',
+					tickDecimals: 0,
+					font : {
+						color : '#bdbdbd'
+					}
+				},
+				xaxis : {
+					tickColor : '#f5f5f5',
+					ticks: data["timestamp"],
+					rotateTicks: 45,
+					font : {
+						color : '#bdbdbd'
+					}
+				},
+				tooltip : true,
+				tooltipOpts : {
+					content : '%s: %y',
+					shifts : {
+						x : -60,
+						y : 25
+					},
+					defaultTheme : false
+				}
+			}
+		);
+	} else $("#ext-humidity-chart2-layout").hide();
+
+	if ($("#checkinth1").prop("checked")) {
+		$("#int-humidity-chart1-layout").show();
 		$.plot($("#int-humidity-chart1"), [{
 		data : data["h1"],
 		label : "Humidity 1",
@@ -246,6 +255,10 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#int-humidity-chart1-layout").hide();
+
+	if ($("#checkinth2").prop("checked")) {
+		$("#int-humidity-chart2-layout").show();
 		$.plot($("#int-humidity-chart2"), [{
 		data : data["h2"],
 		label : "Humidity 2",
@@ -315,6 +328,10 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#int-humidity-chart2-layout").hide();
+
+	if ($("#checkextt1").prop("checked")) {
+		$("#ext-temperature-chart1-layout").show();
 		$.plot($("#ext-temperature-chart1"), [{
 		data : data["extt1"],
 		label : "Exterior temperature 1",
@@ -384,6 +401,10 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#ext-temperature-chart1-layout").hide();
+
+	if ($("#checkextt2").prop("checked")) {
+		$("#ext-temperature-chart2-layout").show();
 		$.plot($("#ext-temperature-chart2"), [{
 		data : data["extt2"],
 		label : "Exterior temperature 2",
@@ -453,6 +474,10 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#ext-temperature-chart2-layout").hide();
+
+	if ($("#checkintt1").prop("checked")) {
+		$("#int-temperature-chart1-layout").show();
 		$.plot($("#int-temperature-chart1"), [{
 		data : data["t1"],
 		label : "Temperature 1",
@@ -522,6 +547,10 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#int-temperature-chart1-layout").hide();
+
+	if ($("#checkintt2").prop("checked")) {
+		$("#int-temperature-chart2-layout").show();
 		$.plot($("#int-temperature-chart2"), [{
 		data : data["t2"],
 		label : "Temperature 2",
@@ -591,77 +620,81 @@ function refreshData(data_in) {
 		        defaultTheme : false
 		    }
 		});
+	} else $("#int-temperature-chart2-layout").hide();
 
-	$.plot($("#light-chart"),
-		[{
-			data : data["light"],
-			label : "Light",
-			color: "#FFB41E"
-		}],
-		{
-			series: {
-				lines : {
-					show : true,
-					fill : true,
-					lineWidth : 2,
-					fillColor : {
-						colors : [{
-							opacity : 0.05
-						}, {
-							opacity : 0.05
-						}]
+	if ($("#checklight").prop("checked")) {
+		$("#light-chart-layout").show();
+		$.plot($("#light-chart"),
+			[{
+				data : data["light"],
+				label : "Light",
+				color: "#FFB41E"
+			}],
+			{
+				series: {
+					lines : {
+						show : true,
+						fill : true,
+						lineWidth : 2,
+						fillColor : {
+							colors : [{
+								opacity : 0.05
+							}, {
+								opacity : 0.05
+							}]
+						}
+					},
+					points: {
+						show: false
+					},
+					shadowSize : 0
+				},
+				grid : {
+					hoverable : true,
+					clickable : true,
+					borderColor : "#f9f9f9",
+					tickColor : "#AAAAAA",
+					borderWidth : 1,
+					labelMargin : 10,
+					backgroundColor : "#fff"
+				},
+				legend : {
+					position : "ne",
+					margin : [0, -24],
+					noColumns : 0,
+					labelBoxBorderColor : null,
+					labelFormatter : function(label, series) {
+						// just add some space to labes
+						return '' + label + '&nbsp;&nbsp;';
+					},
+					width : 30,
+					height : 2
+				},
+				yaxis : {
+					tickColor : '#f5f5f5',
+					tickDecimals: 0,
+					font : {
+						color : '#bdbdbd'
 					}
 				},
-				points: {
-					show: false
+				xaxis : {
+					tickColor : '#f5f5f5',
+					ticks: data["timestamp"],
+					rotateTicks: 45,
+					font : {
+						color : '#bdbdbd'
+					}
 				},
-				shadowSize : 0
-			},
-			grid : {
-				hoverable : true,
-				clickable : true,
-				borderColor : "#f9f9f9",
-				tickColor : "#AAAAAA",
-				borderWidth : 1,
-				labelMargin : 10,
-				backgroundColor : "#fff"
-			},
-			legend : {
-				position : "ne",
-				margin : [0, -24],
-				noColumns : 0,
-				labelBoxBorderColor : null,
-				labelFormatter : function(label, series) {
-					// just add some space to labes
-					return '' + label + '&nbsp;&nbsp;';
-				},
-				width : 30,
-				height : 2
-			},
-			yaxis : {
-				tickColor : '#f5f5f5',
-				tickDecimals: 0,
-				font : {
-					color : '#bdbdbd'
+				tooltip : true,
+				tooltipOpts : {
+					content : '%s: %y',
+					shifts : {
+						x : -60,
+						y : 25
+					},
+					defaultTheme : false
 				}
-			},
-			xaxis : {
-				tickColor : '#f5f5f5',
-				ticks: data["timestamp"],
-				rotateTicks: 45,
-				font : {
-					color : '#bdbdbd'
-				}
-			},
-			tooltip : true,
-			tooltipOpts : {
-				content : '%s: %y',
-				shifts : {
-					x : -60,
-					y : 25
-				},
-				defaultTheme : false
 			}
-		}
-	);
+		);
+	} else $("#light-chart-layout").hide();
 }
